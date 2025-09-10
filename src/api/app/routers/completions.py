70f769fbce30386afb6fd19b5038f1795499404c
Shazil Ahmed <shazil.ahmed@emumba.com>
2025-09-10 10:55:23 +0500

    refactored /completion/chat to semantic kernel

--- src/api/app/routers/completions.py
+++ src/api/app/routers/completions.py
@@ -1,10 +1,17 @@
-from app.functions.chat_functions import ChatFunctions
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from app.functions.chat_functions_semantic_kernel import ChatFunctions
 from app.lifespan_manager import get_chat_client, get_db_connection_pool, get_embedding_client, get_prompt_service
 from app.models import CompletionRequest, CompletionResponse
 from fastapi import APIRouter, Depends
-from langchain.agents import AgentExecutor, create_openai_functions_agent
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.tools import StructuredTool
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.agents import ChatCompletionAgent
+from semantic_kernel.contents.chat_message_content import ChatMessageContent
+
+api_key = os.getenv("API_KEY_AZURE_OPENAI")
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
 
 # Initialize the router
 router = APIRouter(
@@ -17,7 +24,6 @@ router = APIRouter(
 @router.post('/chat', response_model=CompletionResponse)
 async def generate_chat_completion(
     request: CompletionRequest,
-    llm = Depends(get_chat_client),
     db_pool = Depends(get_db_connection_pool),
     embedding_client = Depends(get_embedding_client),
     prompt_service = Depends(get_prompt_service)):
@@ -25,9 +31,6 @@ async def generate_chat_completion(
         
     # Retrieve the copilot prompt
     system_prompt = prompt_service.get_prompt("copilot")
-
-    # Provide the copilot with a persona using the system prompt.
-    messages = [{ "role": "system", "content": system_prompt }]
 
     # Get the chat session ID
     session_id = request.session_id
@@ -39,67 +42,51 @@ async def generate_chat_completion(
         async with db_pool.acquire() as conn:
             session_id = await create_chat_session(conn, request.message)
 
+    # Create a history of the conversation
+    messages = []
+
     # Add the chat history to the messages list for the session
     # Chat history provides context of previous questions and responses for the copilot.
     async with db_pool.acquire() as conn:
-        chat_history = await get_chat_history(conn, session_id)
-        for message in chat_history[-request.max_history:]:
-            messages.append({"role": message["role"], "content": message["content"]})
+        chat_history_from_db = await get_chat_history(conn, session_id)
+        
+        for message in chat_history_from_db[-request.max_history:]:
+            if message["role"] == "system":
+                messages.append(ChatMessageContent(role="system", content=message["content"]))
+            elif message["role"] == "user":
+                messages.append(ChatMessageContent(role="user", content=message["content"]))
+            elif message["role"] == "assistant":
+                messages.append(ChatMessageContent(role="assistant", content=message["content"]))
    
-
-    # Create a chat prompt template
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            MessagesPlaceholder("chat_history", optional=True),
-            ("user", "{input}"),
-            MessagesPlaceholder("agent_scratchpad")
-        ]
-    )
+    # add current user message to history
+    messages.append(ChatMessageContent(role="user", content=request.message))
 
     # Get the chat functions
     cf = ChatFunctions(db_pool, embedding_client)
-
-    # Define tools for the agent to retrieve data from the database
-    tools = [
-        # Hybrid search functions
-        StructuredTool.from_function(coroutine=cf.find_invoice_line_items),
-        StructuredTool.from_function(coroutine=cf.find_invoice_validation_results),
-        StructuredTool.from_function(coroutine=cf.find_milestone_deliverables),
-        StructuredTool.from_function(coroutine=cf.find_sow_chunks_with_semantic_ranking),
-        StructuredTool.from_function(coroutine=cf.find_sow_validation_results),
-        # Get invoice data functions
-        StructuredTool.from_function(coroutine=cf.get_invoice_id),
-        StructuredTool.from_function(coroutine=cf.get_invoice_line_items),
-        StructuredTool.from_function(coroutine=cf.get_invoice_validation_results),
-        StructuredTool.from_function(coroutine=cf.get_invoices),
-        # Get SOW data functions
-        StructuredTool.from_function(coroutine=cf.get_sow_chunks),
-        StructuredTool.from_function(coroutine=cf.get_sow_id),
-        StructuredTool.from_function(coroutine=cf.get_sow_milestones),
-        StructuredTool.from_function(coroutine=cf.get_milestone_deliverables),
-        StructuredTool.from_function(coroutine=cf.get_sow_validation_results),
-        StructuredTool.from_function(coroutine=cf.get_sows),
-        # Get vendor data functions
-        StructuredTool.from_function(coroutine=cf.get_vendors)
-    ]
     
     # Create an agent
-    agent = create_openai_functions_agent(llm=llm, tools=tools, prompt=prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=True)
-    completion = await agent_executor.ainvoke({"input": request.message, "chat_history": messages})
-    completionOutput = completion['output']
+    agent = ChatCompletionAgent(
+        service=AzureChatCompletion(deployment_name=deployment_name,
+        api_key=api_key,
+        endpoint=endpoint),
+        instructions=f"{system_prompt}",
+        plugins=[cf],
+    )
+
+    # response = str((await agent.get_response(f"{history}")).content)
+    response = str((await agent.get_response(messages=messages)).content)
 
     # Write the chat history to the database
     async with db_pool.acquire() as conn:
         await write_chat_history(conn, session_id, "user", request.message)
-        await write_chat_history(conn, session_id, "assistant", completionOutput)
+        await write_chat_history(conn, session_id, "assistant", response)
 
     # Return the completion output
     return CompletionResponse(
-        session_id = session_id,
-        content = completionOutput
+        session_id = int(request.session_id),
+        content = response
     )
+
 
 @router.get('/sessions')
 async def get_chat_sessions(
